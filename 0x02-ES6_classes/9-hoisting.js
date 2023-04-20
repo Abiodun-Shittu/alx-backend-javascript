4af58b1e072c0605c191ad211f605c4d78081b8a
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-04-20 22:32:39 +0100

    Hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,17 +1,16 @@
-
 export class HolbertonClass {
-	constructor(year, location) {
-		this._year = year;
-		this._location = location;
-	}
-	
-	get year() {
-		return this._year;
-	}
-	
-	get location() {
-		return this._location;
-	}
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
 }
 
 export class StudentHolberton {
@@ -43,5 +42,6 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
+const listOfStudents = [student1, student2, student3, student4, student5];
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
