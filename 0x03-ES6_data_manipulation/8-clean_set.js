ae59a55bdd1749c81fb2c6abe8f2652b5f666a46
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-04-29 09:20:59 +0100

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,8 @@
 const cleanSet = (set, startString) => {
-  if (!set || !startString) return '';
-  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  const filteredValues = Array.from(set).filter((value) => typeof value === 'string' && value.startsWith(startString));
   const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
   return cleanedValues.join('-');
 };
