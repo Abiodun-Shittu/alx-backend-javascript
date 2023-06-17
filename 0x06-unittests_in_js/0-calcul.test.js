4325e5e51022d2b44ecaed4026174e22c6dd8194
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-06-16 22:51:23 -0400

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,9 +2,21 @@ import { strictEqual } from "assert";
 import { calculateNumber } from "./0-calcul.js";
 
 describe("calculateNumber", () => {
-	it("should return the sum of rounded numbers", () => {
-		strictEqual(calculateNumber(1.4, 2.6), 4);
-		strictEqual(calculateNumber(0.1, 0.2), 0);
-		strictEqual(calculateNumber(5, 6), 11);
+	it("checks the output", () => {
+		strictEqual(calculateNumber(1, 3), 4);
+		strictEqual(calculateNumber(1, 3.7), 5);
+		strictEqual(calculateNumber(1.2, 3.7), 5);
+		strictEqual(calculateNumber(1.5, 3.7), 6);
+		strictEqual(calculateNumber(3.7, 1), 5);
+		strictEqual(calculateNumber(3.7, 1.2), 5);
+	});
+	it("negative numbers", () => {
+		strictEqual(calculateNumber(-1, 1), 0);
+		strictEqual(calculateNumber(1, -1), 0);
+		strictEqual(calculateNumber(-1, -1), -2);
+	});
+	it("checks arguments", () => {
+		strictEqual(isNaN(calculateNumber(1)), true);
+		strictEqual(isNaN(calculateNumber()), true);
 	});
 });
